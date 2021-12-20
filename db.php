61da8028db321146a41dcd6732f20beec9f57842
Frizth Lyco Tatierra <lycol50@gmail.com>
2021-12-20 14:00:35 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 // modify here below
-$ip = "gsrv.princepines.digital"; // DB Server IP/URL
-$user = "pmns"; // DB Server Username
-$pass = "pmns2021!1937"; // DB Server Password
+$ip = ""; // DB Server IP/URL
+$user = ""; // DB Server Username
+$pass = ""; // DB Server Password
 $port = 3306; // DB Port
 ?>
